cb33ea51604cf4651a97db5b29d9431600586d1a
Rana <ranamohamad215@gmail.com>
2023-12-22 08:49:31 +0200

    Removed some errors.

--- Drafts/Read_Write_Logic.v
+++ Drafts/Read_Write_Logic.v
@@ -2,9 +2,7 @@ module Read_Write_Logic(
     input read_enable,
     input write_enable,
     input chip_select,
-    input Ao,
     input reset, // Active high
-    inout [7:0] Data,
     output reg read_flag,
     output reg write_flag
 );
@@ -13,36 +11,46 @@ module Read_Write_Logic(
 
     always @(posedge reset) begin
         if (reset) begin
-            Data <= 8'b00000000;
             count <= 2'b00;
-            read_flag <= 1'b0;
-            write_flag <= 1'b0;
+            read_flag <= 1'b1;
+            write_flag <= 1'b1;
         end
     end
 
-    always @( chip_select ) begin
+
+  always @( chip_select , read_enable, write_enable) begin
        
          if (~chip_select) begin
+           if(~read_enable & ~write_enable) begin 
+           	    read_flag <= 1'b1;
+                write_flag <=1'b1;
+           end
             // Check for read operation
-            if ( ~ read_enable) begin
-                read_flag <= 1'b1;
+            else if ( ~ read_enable) begin
+                read_flag <= 1'b0;
             end
             // Check for write operation
-            else if (write_enable) begin
+           else if (~ write_enable) begin
                 // Write operation happens after two bytes of low signal
-                if (count >= 2'b10) begin
+             if (count == 2'b10) begin
                     count <= 2'b00;
-                    write_flag <= 1'b1;
                 end else begin
                     count <= count + 1;
                 end
             end
             else begin 
                  // To avoid read and write operations hapenning simultaniously
-                read_flag <= 1'b0;
-                write_flag <=1'b0;
+                read_flag <= 1'b1;
+                write_flag <=1'b1;
             end
         end
     end
+  
+  always@(count)begin
+    if(count ==2)
+       write_flag <= 1'b0;
+    else
+      write_flag <= 1'b1;
+  end
 
 endmodule
