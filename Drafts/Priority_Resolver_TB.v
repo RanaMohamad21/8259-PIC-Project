a43e6302aaec3fdbb4ce6795b547c901f06d3b39
MightyMaya <mayasharaf2@gmail.com>
2023-12-23 21:50:27 +0200

    Added INTFLAG

--- Drafts/Priority_Resolver_TB.v
+++ Drafts/Priority_Resolver_TB.v
@@ -1,31 +1,30 @@
 module Priority_Resolver_tb;
-  reg        INTA;
   reg  [7:0] IRQ_status;
   reg  [7:0] IS_status;
   reg  [7:0] IR_mask;
   reg        Rotating_priority;
-  reg  [7:0] last_serviced;
-  wire [7:0] Priority;
+  reg  [2:0] last_serviced;
+  wire [2:0] PriorityID;
+  wire       INTFLAG;
 
   // Instantiate the Priority_Resolver module
   Priority_Resolver uut (
-    .INTA(INTA),
     .IRQ_status(IRQ_status),
     .IS_status(IS_status),
     .IR_mask(IR_mask),
     .Rotating_priority(Rotating_priority),
     .last_serviced(last_serviced),
-    .Priority(Priority)
+    .PriorityID(PriorityID),
+	.INTFLAG(INTFLAG)
   );
 
   // Initialize inputs
   initial begin
-    INTA = 1'b0;
     IRQ_status = 8'b00000000;
     IS_status = 8'b00000000;
     IR_mask = 8'b00000000;
     Rotating_priority = 1'b0;
-    last_serviced = 8'b00000000;
+    last_serviced = 8'b111;
 
     // Test fully nested mode
     $display("Testing Fully Nested Mode");
@@ -41,7 +40,7 @@ module Priority_Resolver_tb;
     // Test rotating priority mode
     $display("\nTesting Rotating Priority Mode");
     Rotating_priority = 1'b1;
-    last_serviced = 8'b00000001;
+    last_serviced = 8'b000;
     #50; // Simulate for some time
     IS_status = 8'b00000000;
     IRQ_status = 8'b11000000;
@@ -49,7 +48,7 @@ module Priority_Resolver_tb;
     IS_status = 8'b01000000;
     #50  // Simulate for some time
     IS_status = 8'b00000000;
-    last_serviced = 8'b01000000;
+    last_serviced = 8'b110;
     #50  // Simulate for some time
     
     // Add more test scenarios as needed
