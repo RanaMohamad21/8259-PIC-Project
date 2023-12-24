de390e6951f009fdc4cf1bbe29de41b4b6a0cf1a
Somaya Ayman <87386122+Somaya-Ayman@users.noreply.github.com>
2023-12-24 04:12:03 +0200

    Update ISR.v

--- Drafts/ISR.v
+++ Drafts/ISR.v
@@ -1,52 +1,102 @@
 module ISR (
   // Inputs
   input wire [2:0] highest_priority_idx,
-  input wire eoi,
-  input wire aeoi,
-  input wire specific_eoi,  // Signal for specific EOI mode
-  input wire [2:0] specific_irq,  // Specific IRQ for specific EOI
+  input wire AEOI, // 1-> AEOI 0-> EOI
+  input wire specific_eoi_flag,   // Signal for specific EOI mode 1-> specific   0 -> non-specific
+  input wire [2:0] specific_irq,  // Specific IRQ idx for specific EOI
   input wire ack1,
   input wire ack2,
+  input wire SP,
+  input wire SNGL,
   // Outputs
   output reg [7:0] interrupts_in_service,
   output reg [2:0] last_serviced_idx
 );
-  reg [7:0] next_inService_reg  = 8'b0;
 
-always @* begin
-  if (~ack1) begin 
-  //first ack pulse
-    next_inService_reg[highest_priority_idx] <= 1'b1;
-  end 
-  //second ack pusle
-  //ede maya el last serviced
-  //check eoi code
-else begin end
-  if (eoi) begin
-    // Normal EOI
-    next_inService_reg[highest_priority_idx] <= 1'b0;
-    last_serviced_idx <= highest_priority_idx;
-  end 
-else begin end
-  if (aeoi && ~ack2) begin
-    // Automatic EOI
-    next_inService_reg[highest_priority_idx] <= 1'b0;
-    last_serviced_idx <= highest_priority_idx;
-  end 
-else begin end
- if (specific_eoi) begin
-    // Specific EOI
-    if (specific_irq < 8'b1000) begin
-      next_inService_reg[specific_irq] <= 1'b0;
-      last_serviced_idx <= specific_irq;
+  parameter first_eoi = 1'b0;
+  parameter second_eoi = 1'b1;
+
+  reg eoi_prev_state = first_eoi;
+  reg eoi_current_state = first_eoi;
+  reg eoi_next_state = first_eoi;
+
+  reg [7:0] next_inService_reg = 8'b00000000;
+
+  always @(posedge AEOI or posedge specific_eoi_flag) begin
+    if (AEOI) begin
+      eoi_next_state <= ~eoi_current_state;
+    end else begin
+      case (eoi_current_state)
+        first_eoi: eoi_next_state <= second_eoi;
+        second_eoi: eoi_next_state <= first_eoi;
+        default: eoi_next_state <= first_eoi;
+      endcase
     end
-  else begin end
   end
-else begin end
-end
 
-always @* begin
-  interrupts_in_service = next_inService_reg;
-end
+  always @(posedge ack1 or posedge ack2 or posedge AEOI or posedge specific_eoi_flag or posedge highest_priority_idx or posedge SP) begin
+    if (SNGL) begin // Non-cascading mode
+      if (~ack1) begin
+        // First ack pulse
+        next_inService_reg[highest_priority_idx] <= 1'b1;
+      end else if (~AEOI && ~specific_eoi_flag) begin
+        // Normal EOI non-specific
+        next_inService_reg[highest_priority_idx] <= 1'b0;
+        last_serviced_idx <= highest_priority_idx;
+      end else if (AEOI && ~ack2) begin
+        // Automatic EOI
+        next_inService_reg[highest_priority_idx] <= 1'b0;
+        last_serviced_idx <= highest_priority_idx;
+      end else if (~AEOI && specific_eoi_flag) begin
+        // Specific EOI
+        if (specific_irq < 8'b1000) begin
+          next_inService_reg[specific_irq] <= 1'b0;
+          last_serviced_idx <= specific_irq;
+        end
+      end
+    end else begin // Cascading mode
+      if (~ack1) begin
+        // First ack pulse
+        next_inService_reg[highest_priority_idx] <= 1'b1;
+      end else if (AEOI && ~ack2) begin
+        // Automatic EOI
+        next_inService_reg[highest_priority_idx] <= 1'b0;
+        last_serviced_idx <= highest_priority_idx;
+      end else if (SP) begin // Master
+        if (eoi_current_state == first_eoi && ~AEOI && ~specific_eoi_flag) begin
+          // Normal EOI non-specific
+          next_inService_reg[highest_priority_idx] <= 1'b0;
+          last_serviced_idx <= highest_priority_idx;
+        end else if (eoi_current_state == first_eoi && ~AEOI && specific_eoi_flag) begin
+          // Specific EOI
+          if (specific_irq < 8'b1000) begin
+            next_inService_reg[specific_irq] <= 1'b0;
+            last_serviced_idx <= specific_irq;
+          end
+        end
+      end else begin // Slave
+        if (eoi_current_state == second_eoi && ~AEOI && ~specific_eoi_flag) begin
+          // Normal EOI non-specific
+          next_inService_reg[highest_priority_idx] <= 1'b0;
+          last_serviced_idx <= highest_priority_idx;
+        end else if (eoi_current_state == second_eoi && ~AEOI && specific_eoi_flag) begin
+          // Specific EOI
+          if (specific_irq < 8'b1000) begin
+            next_inService_reg[specific_irq] <= 1'b0;
+            last_serviced_idx <= specific_irq;
+          end
+        end
+      end
+    end
+  end
+
+  always @* begin
+    interrupts_in_service <= next_inService_reg;
+  end
+  
+  always @(posedge AEOI) begin
+    eoi_prev_state <= eoi_current_state;
+    eoi_current_state <= eoi_next_state;
+  end
 
 endmodule
