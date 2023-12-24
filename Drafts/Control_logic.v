ce0c404cb4d0ea2aae82bfb950328f624ea7ef88
Mennatallah74 <130947727+Mennatallah74@users.noreply.github.com>
2023-12-24 18:25:37 +0200

    ctrl logic modified

--- Drafts/Control_logic.v
+++ Drafts/Control_logic.v
@@ -1,7 +1,7 @@
 module Control_logic(
   input WD,
   input RD,
-  input CS,
+ 
   input A0,
   input [7:0]IRR,
   input [7:0]ISR,
@@ -11,51 +11,61 @@ module Control_logic(
   output reg ICW1_ADI, //1->4-bit interval,0->b-bit interval 
   output reg ICW1_SNGL, //1->for single controller, 0 for multiple,so ICW3 will be issued
   output reg ICW1_IC4, //1-> ICW4 needed 0-> not needed
-  output reg ICW1_D4, //make it always 1 for ICW1
   output reg ICW4_SFNM,// 1->special fully nested mode is on
   output reg ICW4_BUF, // enable buffered mode
   output reg ICW4_M_OR_S, //master or slave, BUF should be set first
   output reg ICW4_AEOI, //1->auto EOI 
   output reg ICW4_uPM, // 1-> 8086 Mode
-  output reg [7:0] vector_address,
-  output reg [7:0]ICW1,
-  output reg [7:0]ICW3_Master,
-  output reg [7:0]ICW3_Slave,
-  output reg [7:0] ICW4,
-  output reg [7:0] ICW2,
-  input wire [7:0]data_bus,
-  input wire [2:0] highest_priority_ISR,
-  output reg [2:0] reset_by_EOI,
-  output reg [7:0]OCW1,
-  output reg enable_read_IRR_ISR,
-  output reg choose_IRR_ISR,
-  output reg specific_rotate_status,
-  output reg auto_rotate_status,
-  output reg special_mask_mode_status,
-  output reg begin_to_set_ISR,
-  output reg send_ISR_to_data_bus);
+  output reg [7:0] vector_address, //vector address will be sent on the data bus
+  output reg [7:0]ICW3, //ICW3 register
+  output reg [7:0] ICW2, //ICW2 register
+  inout wire [7:0]data_bus, //the  data bus buffer(output during reading, input during writing)
+  input wire [2:0] highest_priority_ISR, //will be determined from the priority resolver.
+  output reg [2:0] reset_by_EOI, //store the ID of the ISR bit that needs to be reset by the ISR.
+  output reg [7:0]OCW1, //interrupt mask register
+  output reg [1:0]reading_status, //determine which register needed to be read(IRR or ISR))
+  output reg auto_rotate_status, //auto rotate mode
+  output reg begin_to_set_ISR, //first ACK
+  output reg send_ISR_to_data_bus); //second ACK
   parameter CONFIG_ICW1 = 3'b000;
   parameter CONFIG_ICW2 = 3'b001;
   parameter CONFIG_ICW3 = 3'b010;
   parameter CONFIG_ICW4 = 3'b011;
-  parameter ALL_ICW_CONFIG_DONE = 3'b011;
+  parameter ALL_ICW_CONFIG_DONE = 3'b100;
   parameter SPECIFIC_EOI = 2'b11;
   parameter NON_SPECIFIC_EOI = 2'b01;
   parameter ROTATE_IN_AEOI = 3'b100;
   parameter  DISABLE_ROTATE_IN_AEOI = 3'b000;
-  parameter ROTATE_NON_SPECIFIC_EOI = 3'b101;
-  parameter ROTATE_SPECIFIC_EOI = 3'b11x;
   parameter CTRL_INITIAL_STATE  = 2'b00;
   parameter FIRST_ACK  = 2'b01;
   parameter SECOND_ACK = 2'b10;
-  
+  reg [7:0]data_bus_container = 8'b00000000;
    
    
   reg[2:0] current_ICW_state  = CONFIG_ICW1;
   reg[2:0] next_ICW_state;
-  
+  initial
+  begin
+    ICW1_LTIM <= 1'b0;
+    ICW1_ADI <= 1'b0;
+    ICW1_SNGL <= 1'b1;
+    ICW1_IC4 <= 1'b0;
+    ICW4_SFNM <= 1'b0;
+    ICW4_BUF <= 1'b0;
+    ICW4_M_OR_S <= 1'b1;
+    ICW4_AEOI <= 1'b0;
+    ICW4_uPM <= 1'b1;
+    vector_address <= 8'b00000000;
+   ICW3 <= 8'b00000000;
+     ICW2 <= 8'b00000000;
+    reset_by_EOI <= 3'b000;
+   OCW1 <= 8'b00000000;
+    auto_rotate_status <= 1'b0;
+    begin_to_set_ISR <= 1'b0;
+    send_ISR_to_data_bus <= 1'b0;
+  end
 
- wire ready_to_config_ICW1;
+   wire ready_to_config_ICW1;
    wire ready_to_config_ICW2;
     wire ready_to_config_ICW3;
     wire ready_to_config_ICW4;
@@ -64,18 +74,19 @@ module Control_logic(
     wire ready_to_config_OCW3;
 
 
-    assign ready_to_config_ICW1 = (current_ICW_state== CONFIG_ICW1) && ~WD && ~A0 && data_bus[4]&& ~CS;
-    assign ready_to_config_ICW2 = (current_ICW_state== CONFIG_ICW2) && ~WD && A0 && ~CS;
-    assign ready_to_config_ICW3 = (current_ICW_state== CONFIG_ICW3) && ~WD && A0 && ~CS;
-    assign ready_to_config_ICW4 = (current_ICW_state==CONFIG_ICW4) && ~WD && A0 && ~CS;
-    assign ready_to_config_OCW1 = (current_ICW_state == ALL_ICW_CONFIG_DONE) && ~WD && ~CS && A0;
-    assign ready_to_config_OCW2 = (current_ICW_state == ALL_ICW_CONFIG_DONE) && ~WD && ~CS && ~A0 && ~data_bus[3] && ~data_bus[4];
-   assign ready_to_config_OCW3 = (current_ICW_state == ALL_ICW_CONFIG_DONE) && ~WD && ~CS && ~A0 && data_bus[3] && ~data_bus[4] && ~data_bus[7];
+    assign ready_to_config_ICW1 = (current_ICW_state == CONFIG_ICW1) && ~WD && ~A0 && data_bus[4];
+    assign ready_to_config_ICW2 = (current_ICW_state == CONFIG_ICW2) && ~WD && A0;
+    assign ready_to_config_ICW3 = (current_ICW_state == CONFIG_ICW3) && ~WD && A0;
+    assign ready_to_config_ICW4 = (current_ICW_state == CONFIG_ICW4) && ~WD && A0;
+    assign ready_to_config_OCW1 = (current_ICW_state == ALL_ICW_CONFIG_DONE) && ~WD && A0;
+    assign ready_to_config_OCW2 = (current_ICW_state == ALL_ICW_CONFIG_DONE) && ~WD && ~A0 && ~data_bus[3] && ~data_bus[4];
+    assign ready_to_config_OCW3 = (current_ICW_state == ALL_ICW_CONFIG_DONE) && ~WD  && ~A0 && data_bus[3] && ~data_bus[4] && ~data_bus[7];
 
 
   reg posedge_INTA;
    reg negedge_INTA;
   reg INTA_old=1;
+  //determining the status of INTA edges
   always@(INTA)
   begin
     posedge_INTA <= ~INTA_old && INTA;
@@ -88,17 +99,18 @@ module Control_logic(
    
  
    wire ready_to_process_interrupts = (current_ICW_state == ALL_ICW_CONFIG_DONE);
+
    
    reg [1:0]state_of_ctrl_logic =  CTRL_INITIAL_STATE;
    reg [1:0]next_state_of_ctrl_logic;
 
-always @(state_of_ctrl_logic,INTA)
+always @(*)
 begin
      begin_to_set_ISR <= (state_of_ctrl_logic==FIRST_ACK);
      send_ISR_to_data_bus <= (state_of_ctrl_logic==SECOND_ACK); 
 end
- 
-   always @(INTA)
+ //FSM for the ctrl logic.
+   always @(INTA,current_ICW_state)
 begin
   if (ready_to_process_interrupts)
   begin
@@ -131,8 +143,7 @@ begin
     
   end
 end
-
-      
+   
    always@(*)
    begin
    if(ready_to_process_interrupts)
@@ -143,91 +154,88 @@ end
 
    
    
-  always @(data_bus) begin 
+  always @(WD) begin 
     case (current_ICW_state)
       CONFIG_ICW1:
       begin
       if(ready_to_config_ICW1)
        begin
-          ICW1_IC4 <=data_bus[0];
-          ICW1_SNGL <=data_bus[1];
-          ICW1_ADI <=data_bus[2];
-          ICW1_LTIM <=data_bus[3];
-          next_ICW_state <= CONFIG_ICW2;
+          ICW1_IC4 =data_bus[0];
+          ICW1_SNGL =data_bus[1];
+          ICW1_ADI =data_bus[2];
+          ICW1_LTIM =data_bus[3];
+          next_ICW_state = CONFIG_ICW2;
        end
      else
        begin
-       ICW1_IC4<= 1'b0;
-       ICW1_SNGL <=1'b1;
-       ICW1_ADI <=1'b0;
-       ICW1_LTIM <=1'b0;
-       next_ICW_state <= CONFIG_ICW1;
+       ICW1_IC4= 1'b0;
+       ICW1_SNGL =1'b1;
+       ICW1_ADI =1'b0;
+       ICW1_LTIM =1'b0;
+       next_ICW_state = CONFIG_ICW1;
        end
      end
       CONFIG_ICW2:
       begin
       if(ready_to_config_ICW2)
         begin
-          ICW2[3] <=data_bus[3];
-          ICW2[4] <=data_bus[4];
-          ICW2[5] <=data_bus[5];
-          ICW2[6] <=data_bus[6];
-          ICW2[7] <=data_bus[7];
-          vector_address[7:3] <= data_bus[7:3];//starting address of interrupts
+          ICW2[3] =data_bus[3];
+          ICW2[4] =data_bus[4];
+          ICW2[5] =data_bus[5];
+          ICW2[6] =data_bus[6];
+          ICW2[7] =data_bus[7];
+          vector_address[7:3] = data_bus[7:3];//starting address of interrupts
         if(ICW1_SNGL)
           begin
          if(ICW1_IC4)
-           next_ICW_state <= CONFIG_ICW4;
+           next_ICW_state = CONFIG_ICW4;
           else
-           next_ICW_state <= ALL_ICW_CONFIG_DONE;
+           next_ICW_state = ALL_ICW_CONFIG_DONE;
          end
          else 
-           next_ICW_state <= CONFIG_ICW3;
+           next_ICW_state = CONFIG_ICW3;
         end
     else
        begin
        ICW2[7:3]  = 5'b00000;
        vector_address[7:3] = 5'b00000;
-       next_ICW_state <= CONFIG_ICW2;
+       next_ICW_state = CONFIG_ICW2;
        end
     end
       CONFIG_ICW3:
       begin
         if(ready_to_config_ICW3)
           begin
-          ICW3_Master[7:0] <=data_bus[7:0];
-          ICW3_Slave[3:0] <=data_bus[3:0];
-          ICW3_Slave[7:4] <= 4'b0000;
+          ICW3[7:0] =data_bus[7:0];
+       
         if(ICW1_IC4)
-          next_ICW_state <= CONFIG_ICW4;
+          next_ICW_state = CONFIG_ICW4;
         else
-          next_ICW_state <= ALL_ICW_CONFIG_DONE;
+          next_ICW_state = ALL_ICW_CONFIG_DONE;
         end
       else
         begin
-        ICW3_Master[7:0] <=8'b00000000;
-          ICW3_Slave[3:0] <=4'b0000;
-          ICW3_Slave[7:4] <= 4'b0000;
-          next_ICW_state <= CONFIG_ICW3;
+        ICW3 = 8'b00000000;
+          next_ICW_state = CONFIG_ICW3;
         end
       end
       CONFIG_ICW4:
       begin
           if(ready_to_config_ICW4)
             begin
-              ICW4_uPM<= data_bus[0];
-              ICW4_AEOI<= data_bus[1];
-              ICW4_M_OR_S<= data_bus[2];
-              ICW4_BUF<= data_bus[3];
-          next_ICW_state <= ALL_ICW_CONFIG_DONE;
+              ICW4_uPM = data_bus[0];
+              ICW4_AEOI = data_bus[1];
+              ICW4_M_OR_S = data_bus[2];
+              ICW4_BUF = data_bus[3];
+          next_ICW_state = ALL_ICW_CONFIG_DONE;
           end
-        else
+          else
           begin
             ICW4_uPM<= 1'b0;
-            ICW4_AEOI<= 1'b0;
+              ICW4_AEOI<= 1'b0;
               ICW4_M_OR_S<= 1'b0;
               ICW4_BUF<= 1'b0;
-              next_ICW_state <= CONFIG_ICW4;
+          next_ICW_state <=CONFIG_ICW4;
           end
       end
       default:
@@ -246,9 +254,9 @@ end
 
 
 //determine masked or unmasked interrupts.
-always@(ready_to_config_OCW1)
+always@(WD)
 begin
-if(ALL_ICW_CONFIG_DONE && ready_to_config_OCW1)
+if(ready_to_config_OCW1 ==1'b1 && WD==1'b0)
   OCW1[7:0] <= data_bus[7:0];
 else
   OCW1[7:0] <= 8'b00000000;
@@ -256,9 +264,9 @@ end
 /* determines which IS to be reset
 /* for specific EOI we take the ID we act upon from the OCW2 register
 * for non-specific EOI we reset the IS which has the highest priority and it will be known from the priority resolver*/
-always@(ready_to_config_OCW2)
+always@(WD)
 begin
-if(ready_to_config_OCW2)
+if(ready_to_config_OCW2 == 1'b1 && WD==1'b0)
   begin
   case(data_bus[6:5])
   SPECIFIC_EOI:
@@ -272,9 +280,9 @@ else
 end
 
 //auto rotate
-always@(ready_to_config_OCW2)
+always@(WD)
 begin
- if(ready_to_config_OCW2)
+ if(ready_to_config_OCW2 == 1'b1 && WD==1'b0)
   case(data_bus[7:5])
     ROTATE_IN_AEOI:
        auto_rotate_status = 1'b1;
@@ -285,56 +293,54 @@ begin
    auto_rotate_status <= 1'b0;
 end
 
-//specific rotate
-always@(ready_to_config_OCW2)
-begin
- if(ready_to_config_OCW2)
-  case(data_bus[7:5])
-  ROTATE_NON_SPECIFIC_EOI:
-       specific_rotate_status = 1'b1;
-    ROTATE_SPECIFIC_EOI:
-       specific_rotate_status= 1'b0;
-  endcase
- else
-   specific_rotate_status <= 1'b0;
-end
 
-always@(ready_to_config_OCW3)
-begin
-  if(ready_to_config_OCW3 && data_bus[6])
-    special_mask_mode_status<= data_bus[5]; //there is two modes, special and not special
-  else
-    special_mask_mode_status <=1'b0;
-end
 
-always@(ready_to_config_OCW3)
+
+
+always@(WD)
 begin
 
 
- if(ready_to_config_OCW3)
+ if(ready_to_config_OCW3 ==1'b1 &&  WD==1'b0)
     begin
-        enable_read_IRR_ISR <= data_bus[1];
-        choose_IRR_ISR <= data_bus[0];
+        reading_status[1:0]=data_bus[1:0];
+        if (reading_status == 2'b10 && RD==1'b0)
+        begin
+            data_bus_container = IRR;
+        end 
+        else if (reading_status == 2'b11 && RD==1'b0)
+        begin
+           data_bus_container = ISR;
+        end
+        else  
+        begin
+         end
     end
 else
-    begin
-    enable_read_IRR_ISR <= 1'b0;
-    choose_IRR_ISR <= 1'b0;
-    
-    end
+    begin 
+        reading_status[1:0]= 2'b00;
+     end
     
 end
 
 
-
-
-always@(IRR)
+always@(RD)
 begin
-if(IRR !=8'b00000000)
-INT<=1'b1;
-else
-INT<=1'b0;
+
+  if(send_ISR_to_data_bus == 1'b1 && RD==1'b0)
+  begin
+   vector_address[2:0] = highest_priority_ISR; //bad ID
+   data_bus_container = vector_address;
+   send_ISR_to_data_bus = 1'b0;
+
+  end
 end
+
+
+
+
+assign data_bus = (RD==1'b0)?data_bus_container:(8'bzzzzzzzz);
+
 
 
 /*needed to be done: sending the vector address to the data bus*/
--- Drafts/Control_logic_tb.v
+++ Drafts/Control_logic_tb.v
@@ -6,18 +6,20 @@ module Control_logic_tb;
   parameter CLK_PERIOD = 10; // Clock period in ns
 
   // Signals
-  reg clk, rst, WD, RD, CS, A0, INTA;
-  reg [7:0] IRR, ISR, data_bus;
-  wire INT, ICW1_LTIM, ICW1_ADI, ICW1_SNGL, ICW1_IC4, ICW1_D4, ICW4_SFNM, ICW4_BUF, ICW4_M_OR_S, ICW4_AEOI, ICW4_uPM;
-  wire [7:0] vector_address, ICW1, ICW3, ICW4, ICW2, OCW1;
+  reg clk, WD, RD, A0, INTA;
+  reg [7:0] IRR, ISR;
+  reg[2:0] highest_priority_ISR;
+  wire[7:0] data_bus;
+  wire INT, ICW1_LTIM, ICW1_ADI, ICW1_SNGL, ICW1_IC4, ICW4_SFNM, ICW4_BUF, ICW4_M_OR_S, ICW4_AEOI, ICW4_uPM;
+  wire [7:0] vector_address, ICW3, ICW2, OCW1;
   wire [2:0] reset_by_EOI;
-  wire enable_read_IRR_ISR, choose_IRR_ISR, auto_rotate_status, special_mask_mode_status, begin_to_set_ISR, send_ISR_to_data_bus;
-
+  wire  auto_rotate_status, begin_to_set_ISR, send_ISR_to_data_bus;
+  wire [1:0] reading_status;
+  reg [7:0]data_bus_container;
   // Instantiate the Control_logic module
   Control_logic uut (
     .WD(WD),
     .RD(RD),
-    .CS(CS),
     .A0(A0),
     .IRR(IRR),
     .ISR(ISR),
@@ -27,26 +29,20 @@ module Control_logic_tb;
     .ICW1_ADI(ICW1_ADI),
     .ICW1_SNGL(ICW1_SNGL),
     .ICW1_IC4(ICW1_IC4),
-    .ICW1_D4(ICW1_D4),
     .ICW4_SFNM(ICW4_SFNM),
     .ICW4_BUF(ICW4_BUF),
     .ICW4_M_OR_S(ICW4_M_OR_S),
     .ICW4_AEOI(ICW4_AEOI),
     .ICW4_uPM(ICW4_uPM),
     .vector_address(vector_address),
-    .ICW1(ICW1),
-    .ICW3_Master(ICW3),
-    .ICW3_Slave(ICW3),
-    .ICW4(ICW4),
+    .ICW3(ICW3),
     .ICW2(ICW2),
     .data_bus(data_bus),
-    .highest_priority_ISR(3'b000), // Provide a default value for highest_priority_ISR
+    .highest_priority_ISR(highest_priority_ISR), // Provide a default value for highest_priority_ISR
     .reset_by_EOI(reset_by_EOI),
     .OCW1(OCW1),
-    .enable_read_IRR_ISR(enable_read_IRR_ISR),
-    .choose_IRR_ISR(choose_IRR_ISR),
+    .reading_status(reading_status),
     .auto_rotate_status(auto_rotate_status),
-    .special_mask_mode_status(special_mask_mode_status),
     .begin_to_set_ISR(begin_to_set_ISR),
     .send_ISR_to_data_bus(send_ISR_to_data_bus)
   );
@@ -55,6 +51,8 @@ module Control_logic_tb;
   initial begin
     clk = 0;
     forever #((CLK_PERIOD)/2) clk = ~clk;
+    
+    
   end
 
   // Test stimulus
@@ -63,64 +61,64 @@ module Control_logic_tb;
    
     WD = 1'b1;
     RD = 1'b1;
-    CS = 1'b1;
+   
     A0 = 1'b0;
     IRR = 8'b0;
     ISR = 8'b0;
     INTA = 1'b1;
-    data_bus = 8'b0;
+    data_bus_container = 8'b0;
     
      //ICW1
      #10 WD = 1'b0;
-     #10CS= 1'b0;
-     #10A0 = 1'b0;
-     #10 data_bus= 8'b00010101;
+     
+     A0 = 1'b0;
+     data_bus_container= 8'b00010101;
     //ICW2
-    #100WD = 1'b1;
-    #100CS = 1'b1;
-     #10 WD = 1'b0;
-     #10 CS = 1'b0;
-     #10 A0 =1'b1;
-     #10 data_bus = 8'b11111000;
+    #10 WD = 1'b1;
+       
+        A0=1'b0;
+   #10 WD = 1'b0;
+  
+    A0 =1'b1;
+      data_bus_container = 8'b11111000;
      //ICW3
-    #100WD = 1'b1;
-    #100CS = 1'b1;
-     #10 WD = 1'b0;
-     #10 CS = 1'b0;
-     #10 A0 =1'b1;
-     #10 data_bus = 8'b11111111;
+    #10 WD = 1'b1;
+ 
+    #10 WD = 1'b0;
+ 
+      A0 =1'b1;
+     data_bus_container = 8'b11111111;
 //ICW4
-     #100WD = 1'b1;
-     #100CS = 1'b1;
-     #10 WD = 1'b0;
-     #10 CS = 1'b0;
-     #10 A0 =1'b1;
-     #10 data_bus = 8'b00011111;
+     #10 WD = 1'b1;
+    
+     A0=1'b0;
+    #10 WD = 1'b0;
+   
+    A0 =1'b1;
+     data_bus_container = 8'b00011111;
      
      
  
-     #100 WD = 1'b1;
-     #100 CS = 1'b1;
+     #10 WD = 1'b1;
+    
 //trying to send IRR
-     #10 IRR = 8'b01000000;
-     #10 INTA = 0;
-     #100 INTA = 1;
-     #10 INTA = 0;
-     #100 INTA = 1;
 
      #10 IRR = 8'b01100000;
+     #10 highest_priority_ISR =3'b111;
      #10 INTA = 0;
      #100 INTA = 1;
      #10 INTA = 0;
      #100 INTA = 1;
-    
- 
+   
+    #10 RD= 1'b0;
+    #10 RD = 1'b1;
 
 
 
     #100 $stop; // Stop simulation after some time
   end
-
+ 
+  assign data_bus = (RD==1'b0)?vector_address:(WD==1'b0)? data_bus_container:8'bzzzzzzzz;
   // Display outputs
   always @(posedge clk) begin
     $display("Time=%0t: INT=%b, vector_address=%h, OCW1=%h", $time, INT, vector_address, OCW1);
