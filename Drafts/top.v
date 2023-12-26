eb6065b848987f62a1e6bde4d40aeebe451b067a
Somaya Ayman <87386122+Somaya-Ayman@users.noreply.github.com>
2023-12-26 03:09:34 +0200

    Update top.v

--- Drafts/top.v
+++ Drafts/top.v
@@ -1,9 +1,9 @@
 
 module top(
-   input wire [7:0]  IRQ_status,
-   input wire [7:0]interrupt_inservice,
-  input wire [2:0]  last_serviced,
-  input wire [2:0] PriorityID,
+   output wire [7:0]  IRQ_status,
+   output wire [7:0]interrupt_inservice,
+  output wire [2:0]  last_serviced,
+  output wire [2:0] PriorityID,
   inout [7:0]data_Bus,
   inout [2:0] cascade_lines,
   
@@ -13,8 +13,11 @@ module top(
     input chip_select,
     input slave_program,
     input INTA,
-  input [7:0] interrupt_requests,
-    output INT);
+    input [7:0] interrupt_requests,
+    output INT,
+    output wire first_ack,
+    output wire second_ack
+    );
 
 // Instantiate internal signals
   // Interrupt requests from IRR
@@ -24,14 +27,13 @@ module top(
       //the last serviced priority in ISR
 
    //Selected Priority given to cascade and ISR
-reg       INTFLAG;      //interrupt flag given to control
-wire edge_level_trigger; // controller + IRQ
-wire AEOI; // controller + ISR 
+  reg       INTFLAG;      //interrupt flag given to control
+  wire edge_level_trigger; // controller + IRQ
+  wire AEOI; // controller + ISR 
 
-wire first_ack;
-wire second_ack;
-wire INT_Flag;
-/**
+
+  wire INT_Flag;
+ /**
     Read/Write + Control
 */
 wire reset;
@@ -48,7 +50,7 @@ wire vecFlag;
 Control_logic control_logic(.WD(write_flag),.RD(read_flag),.A0(A0),.IRR(IRQ_status),.ISR(interrupt_inservice),.INTA(INTA),.INT(INT),.ICW1_LTIM(edge_level_trigger),.ICW1_SNGL(single_or_cascade),.ICW4_AEOI(AEOI),.data_bus(data_Bus), .highest_priority_ISR(PriorityID),.reset_by_EOI(EOI_specific_IRQ_index),.OCW1(IR_mask),.reading_status(reading_status),.auto_rotate_status(Rotating_priority),.specific_eoi_status(specific_eoi_flag),.begin_to_set_ISR(first_ack),.send_ISR_to_data_bus(second_ack),.slave_id(slave_id),.vecFlag(vecFlag),.INT_Flag(INT_Flag));
 
 
-  ISR isr(.AEOI(AEOI),.interrupts_in_service(interrupt_inservice),.last_serviced_idx(last_serviced),.specific_irq(EOI_specific_IRQ_index),.ack1(first_ack),.ack2(second_ack),.SP(slave_program),.SNGL(single_or_cascade),.specific_eoi_flag(specific_eoi_flag),.highest_priority_idx(PriorityID));
+ISR isr(.AEOI(AEOI),.interrupts_in_service(interrupt_inservice),.last_serviced_idx(last_serviced),.specific_irq(EOI_specific_IRQ_index),.ack1(first_ack),.ack2(second_ack),.SP(slave_program),.SNGL(single_or_cascade),.specific_eoi_flag(specific_eoi_flag),.highest_priority_idx(PriorityID));
 
 
 IRQs irqs(.irq_lines(interrupt_requests),.irq_status(IRQ_status),.trigger(edge_level_trigger),.inta(first_ack),.highest_priority_idx(PriorityID));
